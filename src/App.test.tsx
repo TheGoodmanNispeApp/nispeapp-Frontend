f388cc1bf4930e6881329f4df08d212edb75565e
TheGuzman <guzmanmedrano.diego@gmail.com>
2023-07-01 15:50:09 +0200

    feat(provider): add provider to app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
 
 test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
+  const { baseElement } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   expect(baseElement).toBeDefined();
 });
